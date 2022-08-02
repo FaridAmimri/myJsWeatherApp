7e138c709ece94aa84a2ccd0f7ee0b4be21ecb34
Farid Amimri <farid.amimri@gmail.com>
2022-08-02 11:43:42 +0200

    Fetch weather with Api + Display results

---
+++ script.js
@@ -0,0 +1,20 @@
+let weather = {
+    "apiKey": "22519734f5421c8f55450aaa03737461",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+    }
+}
