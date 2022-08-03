e7918e9be4e42100f4389f2d2ece300edbec4137
Farid Amimri <farid.amimri@gmail.com>
2022-08-03 12:24:31 +0200

    Add Key press listener

--- script.js
+++ script.js
@@ -40,3 +40,7 @@ let weather = {
 document.querySelector('.search button').addEventListener('click', function () {
     weather.search()
 })
+
+document.querySelector('.search-bar').addEventListener('keyup', function (event) {
+    if (event.key == 'Enter') { weather.search() }
+})
